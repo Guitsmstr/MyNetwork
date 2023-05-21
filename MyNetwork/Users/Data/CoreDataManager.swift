2de170b490a0ac926fed12e7efc77c050cca1bf4
Guillermo <guillermovrc10@gmail.com>
2023-05-21 16:07:20 -0500

    - convert coreData methods to combine and add save function to save APIUsers

--- MyNetwork/Users/Data/CoreDataManager.swift
+++ MyNetwork/Users/Data/CoreDataManager.swift
@@ -6,13 +6,14 @@
 //
 
 import Foundation
-
+import Combine
 import CoreData
 
 protocol CoreDataManagerProtocol {
-    func fetchUsers(by searchText: String?) throws -> [MNUser]
+    func fetchUsers(by searchText: String?) -> AnyPublisher<[MNUser], Error>
     var context: NSManagedObjectContext { get }
     func saveContext() throws
+    func save(users: [APIUser]) -> AnyPublisher<[MNUser], Error>
 }
 
 final class CoreDataManager: CoreDataManagerProtocol {
@@ -22,7 +23,7 @@ final class CoreDataManager: CoreDataManagerProtocol {
     private init() {}
     
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "YourModelName")
+        let container = NSPersistentContainer(name: "MyNetwork")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
@@ -35,13 +36,30 @@ final class CoreDataManager: CoreDataManagerProtocol {
         return persistentContainer.viewContext
     }
     
-    func fetchUsers(by searchText: String? = nil) throws -> [MNUser] {
-        let fetchRequest: NSFetchRequest<MNUser> = MNUser.fetchRequest()
-        if let searchText = searchText, !searchText.isEmpty {
-            fetchRequest.predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchText)
-        }
-        let users = try context.fetch(fetchRequest)
-        return users
+    func fetchUsers(by searchText: String? = nil) -> AnyPublisher<[MNUser], Error> {
+        return Deferred {
+            Future { promise in
+                let fetchRequest: NSFetchRequest<MNUser> = MNUser.fetchRequest()
+                if let searchText = searchText, !searchText.isEmpty {
+                    fetchRequest.predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchText)
+                }
+                do{
+                    let users = try self.context.fetch(fetchRequest)
+                    promise(.success(users))
+                }
+                catch{
+                    promise(.failure(error))
+                }
+            }
+        }.eraseToAnyPublisher()
+        
+        
+//        let fetchRequest: NSFetchRequest<MNUser> = MNUser.fetchRequest()
+//        if let searchText = searchText, !searchText.isEmpty {
+//            fetchRequest.predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchText)
+//        }
+//        let users = try context.fetch(fetchRequest)
+//        return users
         
     }
     
@@ -49,6 +67,37 @@ final class CoreDataManager: CoreDataManagerProtocol {
         if context.hasChanges {
             try context.save()
         }
+    }
+    
+    func save(users: [APIUser]) -> AnyPublisher<[MNUser], Error> {
+        Deferred {
+            Future { [weak self] promise in
+                guard let self = self else {
+                    return
+                }
+                
+                var savedUsers = [MNUser]()
+                
+                users.forEach { apiUser in
+                    let user = MNUser(context: self.context)
+                    user.id = Int64(apiUser.id)
+                    user.name = apiUser.name
+                    user.email = apiUser.email
+                    user.phone = apiUser.phone
+                    
+                    savedUsers.append(user)
+                }
+                
+                do {
+                    try self.context.save()
+                    promise(.success(savedUsers))
+                } catch {
+                    self.context.rollback()
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
     }
 }
 
